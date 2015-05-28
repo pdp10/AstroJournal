84e646b508cc200b00c56f1a886fb25c3fe2586b
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2015-05-28 23:34:09 +0100

    added controls
    Former-commit-id: 087df10e05836e317473ea385ee3400c49034ce2
    Former-commit-id: eedc63b6217f3daed5bb59aa87a4179863d01fb8

--- create_journal.sh
+++ create_journal.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
+input_folder="tsv_reports"
+output_folder="latex_reports"
+aj_latex_file="astrojournal.tex"
+
+
+# Check if input_folder exists
+if [ ! -d ${input_folder} ]; then
+    printf "Folder ${input_folder} does not exist. Aborting.\n";
+    exit 1;
+fi
+
+# Check if output_folder_exists
+if [ ! -d ${output_folder} ]; then
+    printf "Folder ${output_folder} does not exist. Aborting.\n";
+    exit 1;
+fi
+
+
+# Remove previous aj_latex_file file 
+rm -f ${aj_latex_file}
+
+
+
 
 # Run AstroJournal and generate the Latex code
-java -jar astrojournal-*.jar tsv_files observations
+java -jar astrojournal-*.jar ${input_folder} ${output_folder}
+
+
+# Check whether astrojournal.tex is empty (has not been created correctly)
+if [ ! -s ${aj_latex_file} ]; then
+    rm -f ${aj_latex_file}
+    printf "File ${aj_latex_file} was not created. Aborting.\n";
+    exit 1;
+fi
+
 
 
 # Generate the PDF file from the Latex code generated previously
