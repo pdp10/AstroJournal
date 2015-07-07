94e3e450d4f8a5233e26947d831cc5fc06315938
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2015-07-07 20:10:31 +0100

    Moved files into packages
    Former-commit-id: 3f49bcd357272e35ef5246c24dae40966e346de1
    Former-commit-id: b39e91eea8c555e8b4350c78e3dabbabb4b2b697

--- src/org/astrojournal/observation/AJObservationItem.java
+++ src/org/astrojournal/observation/AJObservationItem.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with AstroJournal.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.astrojournal;
+package org.astrojournal.observation;
 
 
 /** 
