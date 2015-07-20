2386ad00142dbfeeb532e5a242938af70e7495fe
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2015-07-20 22:52:01 +0100

    uploaded last observation and catalogue records

--- upload.sh
+++ upload.sh
@@ -20,9 +20,15 @@
 # upload the last observation to my git repository
 git add astrojournal.pdf
 git add astrojournal.tex
+git add astrojournal.tsv
+git add latex_header_footer/*.tex
 git add latex_reports/*.tex
-git add tsv_reports/*.tsv
 git add latex_catalogues/*.tex
+git add tsv_reports/*.tsv
 git add tsv_catalogues/*.tsv
 git commit -m 'uploaded last observation and catalogue records'
 git push origin master
+
+
+# upload the pdf to the website
+
