e8be61f136c3a08b46925f6fe3114eb6e4b16d65
pdp10 <piero.dallepezze@gmail.com>
2015-12-12 23:59:51 +0000

    This version creates the package correctly.

--- src/main/java/org/astrojournal/gui/AJMiniGUI.java
+++ src/main/java/org/astrojournal/gui/AJMiniGUI.java
@@ -85,7 +85,7 @@ public class AJMiniGUI extends JFrame {
     
     // Configure AJMiniGUI with basic parameters
     setTitle(AJConfig.getInstance().applicationName + " " + AJConfig.getInstance().applicationVersion);
-    setIconImage(new ImageIcon(ClassLoader.getSystemResource("src/resources/aj_icon.png")).getImage());
+    setIconImage(new ImageIcon(ClassLoader.getSystemResource("aj_icon.png")).getImage());
     setSize(600,600);
     setMinimumSize(new Dimension(480, 300));
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
