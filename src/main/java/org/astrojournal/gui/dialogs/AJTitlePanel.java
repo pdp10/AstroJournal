b64928b7eebc8187eceedbca557bcf28ea783eed
pdp10 <piero.dallepezze@gmail.com>
2015-12-13 14:28:43 +0000

    Added logo

--- src/main/java/org/astrojournal/gui/dialogs/AJTitlePanel.java
+++ src/main/java/org/astrojournal/gui/dialogs/AJTitlePanel.java
@@ -22,13 +22,18 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.RenderingHints;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
 import org.astrojournal.AJConfig;
 
 /**
  * A simple title panel to return minimal information about AstroJournal.
+ * 
  * @author Piero Dalle Pezze
  * @version $Rev$
  * @since 1.0
@@ -36,89 +41,104 @@ import org.astrojournal.AJConfig;
  */
 public class AJTitlePanel extends JPanel {
 
-  private static final long serialVersionUID = 8192425675694492246L;
+    private static final long serialVersionUID = 8192425675694492246L;
 
-  private JTextField website;
-
-  /**
-   * Provides a small panel which gives details of the AstroJournal version
-   * and copyright.  Used in both the welcome panel and the about dialog.
-   */
-  public AJTitlePanel () {
-    initComponents();
-  }
-
-  /** This method is called from within the constructor to
-   * initialize the form. */
-  private void initComponents() {
-    setLayout(new BorderLayout(5,1));
-
-    JPanel c = new JPanel();
-    c.setLayout(new GridBagLayout());
-
-    GridBagConstraints constraints = new GridBagConstraints();
-    constraints.gridx=1;
-    constraints.gridy=1;
-    constraints.weightx = 1;
-    constraints.weighty=1;
-    constraints.insets = new Insets(3, 3, 0, 0);
-    constraints.fill = GridBagConstraints.NONE;
-
-    JLabel program = new SmoothJLabel(
-      AJConfig.getInstance().applicationName + " " + AJConfig.getInstance().applicationVersion,
-      JLabel.CENTER);
-    program.setFont(new Font("Dialog",Font.BOLD,18));
-    program.setForeground(new Color(0,0,200));
-    c.add(program,constraints);
-
-    constraints.gridy++;
-    website = new JTextField("http://pdp10.github.io/AstroJournal/");
-    website.setFont(new Font("Dialog",Font.PLAIN,14));
-    website.setEditable(false);
-    website.setBorder(null);
-    website.setOpaque(false);
-    website.setHorizontalAlignment(JTextField.CENTER);
-    c.add(website,constraints);
-    constraints.gridy++;
-
-    JLabel copyright = new JLabel("<html>\u00a9 Piero Dalle Pezze 2015-16</html>", JLabel.CENTER);
-    copyright.setFont(new Font("Dialog",Font.PLAIN,14));
-    c.add(copyright,constraints);
-    constraints.gridy++;
-
-    add(c,BorderLayout.CENTER);
-  }
-
-  /**
-   * A JLabel with anti-aliasing enabled.  Takes the same constructor
-   * arguments as JLabel
-   */
-  private class SmoothJLabel extends JLabel {
-
-    private static final long serialVersionUID = 4036739763854114821L;
+    private JTextField website;
 
     /**
-     * Creates a new label
-     * 
-     * @param text The text
-     * @param position The JLabel constant position for alignment
+     * Provides a small panel which gives details of the AstroJournal version
+     * and copyright. Used in both the welcome panel and the about dialog.
      */
-    public SmoothJLabel (String text, int position) {
-      super(text,position);
+    public AJTitlePanel() {
+	initComponents();
     }
 
-    /* (non-Javadoc)
-     * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+    /**
+     * This method is called from within the constructor to initialise the form.
      */
-    @Override
-    public void paintComponent (Graphics g) {
-      if (g instanceof Graphics2D) {
-        ((Graphics2D)g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-      }
-      super.paintComponent(g);
+    private void initComponents() {
+	setLayout(new BorderLayout(5, 1));
+
+	ImageIcon logo = new ImageIcon(
+		ClassLoader.getSystemResource("aj_icon.png"));
+	JPanel logoPanel = new JPanel();
+	logoPanel.add(new JLabel("", logo, JLabel.CENTER));
+	logoPanel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
+	add(logoPanel, BorderLayout.WEST);
+
+	JPanel c = new JPanel();
+	c.setLayout(new GridBagLayout());
+
+	GridBagConstraints constraints = new GridBagConstraints();
+	constraints.gridx = 1;
+	constraints.gridy = 1;
+	constraints.weightx = 1;
+	constraints.weighty = 1;
+	constraints.insets = new Insets(3, 3, 0, 0);
+	constraints.fill = GridBagConstraints.NONE;
+
+	JLabel program = new SmoothJLabel(
+		AJConfig.getInstance().applicationName + " "
+			+ AJConfig.getInstance().applicationVersion,
+		JLabel.CENTER);
+	program.setFont(new Font("Dialog", Font.BOLD, 18));
+	program.setForeground(new Color(0, 0, 200));
+	c.add(program, constraints);
+
+	constraints.gridy++;
+	website = new JTextField("http://pdp10.github.io/AstroJournal/");
+	website.setFont(new Font("Dialog", Font.PLAIN, 14));
+	website.setEditable(false);
+	website.setBorder(null);
+	website.setOpaque(false);
+	website.setHorizontalAlignment(JTextField.CENTER);
+	c.add(website, constraints);
+	constraints.gridy++;
+
+	JLabel copyright = new JLabel(
+		"<html>\u00a9 Piero Dalle Pezze 2015-16</html>", JLabel.CENTER);
+	copyright.setFont(new Font("Dialog", Font.PLAIN, 14));
+	c.add(copyright, constraints);
+	constraints.gridy++;
+
+	add(c, BorderLayout.CENTER);
     }
 
-  }
-  
+    /**
+     * A JLabel with anti-aliasing enabled. Takes the same constructor arguments
+     * as JLabel
+     */
+    private class SmoothJLabel extends JLabel {
+
+	private static final long serialVersionUID = 4036739763854114821L;
+
+	/**
+	 * Creates a new label
+	 * 
+	 * @param text
+	 *            The text
+	 * @param position
+	 *            The JLabel constant position for alignment
+	 */
+	public SmoothJLabel(String text, int position) {
+	    super(text, position);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+	 */
+	@Override
+	public void paintComponent(Graphics g) {
+	    if (g instanceof Graphics2D) {
+		((Graphics2D) g).setRenderingHint(
+			RenderingHints.KEY_ANTIALIASING,
+			RenderingHints.VALUE_ANTIALIAS_ON);
+	    }
+	    super.paintComponent(g);
+	}
+
+    }
 
 }
