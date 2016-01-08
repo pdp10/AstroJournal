4dd8d66aacf534b9caced5a1ba17962b08b25529
pdp10 <piero.dallepezze@gmail.com>
2016-01-08 18:42:29 +0000

    Added jUnit AllTests runner.

---
+++ src/test/java/main/AllTests.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2015 Piero Dalle Pezze
+ *
+ * This file is part of AstroJournal.
+ *
+ * AstroJournal is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+/*
+ * Changelog: 
+ * - Piero Dalle Pezze: Class creation.
+ */
+package main;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	ImportObservationTest.class,
+	RunSamplesTest.class,
+	})
+
+public class AllTests { }
