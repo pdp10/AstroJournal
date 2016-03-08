abb2fa5fa327457fd7c09fe78452cbe974c615e3
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2016-03-08 20:47:56 +0000

    Linux script is now executable from every location.

--- astrojournal.sh
+++ astrojournal.sh
@@ -18,6 +18,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+# the folder containing the script becomes root.
+cd $(dirname $0)
+
 
 # Note: AstroJournal also accepts -D java options (e.g. aj.raw_reports) 
 # as input parameters.
