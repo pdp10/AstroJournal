fb899cfa55e598e3ef200d13189dfc71426068b7
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2016-03-23 23:52:14 +0000

    Updated dependency information and executables.

--- src/main/java/org/astrojournal/gui/dialogs/LicenseDialog.java
+++ src/main/java/org/astrojournal/gui/dialogs/LicenseDialog.java
@@ -19,8 +19,7 @@
  */
 /*
  * Changelog:
- * - Piero Dalle Pezze: Code taken from the class LicenseDialog.java in 
- * the software BamQC (GPL v3). Code merged and adapted for AstroJournal. Added internationalisation and close button.
+ * - Piero Dalle Pezze: Class creation
  */
 package org.astrojournal.gui.dialogs;
 
