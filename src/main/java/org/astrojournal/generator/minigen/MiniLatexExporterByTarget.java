d0601bd8ccde662d4d015ce2c99258195454a028
Piero Dalle Pezze <pdp10@users.noreply.github.com>
2016-06-03 20:21:05 +0100

    Obsolete todos (#78)
    * Removed obsolete TODO
    
    Former-commit-id: 269ada8c161f627e99e25454ba3d6be4da3a3833

--- src/main/java/org/astrojournal/generator/minigen/MiniLatexExporterByTarget.java
+++ src/main/java/org/astrojournal/generator/minigen/MiniLatexExporterByTarget.java
@@ -232,7 +232,6 @@ public class MiniLatexExporterByTarget extends LatexExporterByTarget {
     @Override
     public void writeLatexContent(Writer writer, Report report)
 	    throws IOException {
-	// TODO Auto-generated method stub
     }
 
 }
